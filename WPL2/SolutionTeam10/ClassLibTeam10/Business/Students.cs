1229f0c4b8604e1e4e3b030a80986bcb2f1bff36
Jasper Orens <12202416@student.pxl.be>
2023-05-02 07:39:06 +0200

    new

---
+++ WPL2/SolutionTeam10/ClassLibTeam10/Business/Students.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibTeam10.Business.Entities;
+using ClassLibTeam10.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibTeam10.Business
+{
+    public static class Students
+    {
+        public static IEnumerable<Student> List() 
+        {
+            return StudentRepository.StudentList; 
+        }
+        public static void Add(string firstName, string lastName)
+        {
+            StudentRepository.Add(firstName, lastName);
+        }
+    }
+}
---
+++ WPL2/SolutionTeam10/ClassLibTeam10/Data/Framework/Settings.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibTeam00.Data.Framework
+{
+    static class Settings
+    {
+        public static string GetConnectionString()
+        {
+            String connectionString = "Trusted_Connection = True;";
+
+            connectionString += $@"Server=DESKTOP-26A9125\SQLEXPRESS;";
+            connectionString += $"Database=DB_Team10;";
+            return connectionString;
+        }
+    }
+}
