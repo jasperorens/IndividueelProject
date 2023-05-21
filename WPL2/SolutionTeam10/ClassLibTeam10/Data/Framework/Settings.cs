d1f07f82595292a42af47890240fb8ff47385301
jasper <12202416@student.pxl.be>
2023-05-21 19:34:39 +0200

    finish

--- WPL2/SolutionTeam10/ClassLibTeam10/Data/Framework/Settings.cs
+++ WPL2/SolutionTeam10/ClassLibTeam10/Data/Framework/Settings.cs
@@ -8,7 +8,7 @@ namespace ClassLibTeam00.Data.Framework
         {
             String connectionString = "Trusted_Connection = True;";
             //DESKTOP-26A9125\SQLEXPRESS
-            connectionString += $@"Server=5CG21505WF\SQLEXPRESS;";
+            connectionString += $@"Server=DESKTOP-0CES6DI\SQLEXPRESS;";
             connectionString += $"Database=DB_Team10;";
             return connectionString;
         }
